bde2a3fd75145fd0f91514ec1ba93f2887af991a
SS <shavarshsimonyan1@gmail.com>
2021-02-06 23:35:11 +0400

    removed unneccerary code from unit tests

--- unit_test/main.cpp
+++ unit_test/main.cpp
@@ -1,16 +1,7 @@
-#include <cmath>
 #include <cassert>
 #include <gtest/gtest.h>
 
 #include "../db_handling/dbData.h"
- 
-TEST(SquareRootTest, PositiveNos) 
-{ 
-    assert((6 == std::sqrt(36.0)));
-    assert((18.0 == std::sqrt(324.0)));
-    assert((25.4 == std::sqrt(645.16)));
-    assert((0 == std::sqrt(0.0)));
-}
  
 TEST(DBConnection, ConnectionSuccess) 
 { 
